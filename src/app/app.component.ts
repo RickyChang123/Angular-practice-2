20810c0a3b52c537075a7b79734c3550fe96d6c5
DYNABOOK-LAB\rickychang <ricky10455130@gmail.com>
2024-07-31 15:20:22 +0800

    7/31更新完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import type { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,23 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, inject, input, Input } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
+
+  //signal
+  results = computed(() => this.investmentService.resultsData());
+  // results = this.investmentService.resultsData.asReadonly();
+
+  //不是signal
+  // get results() {
+  //   return this.investmentService.resultsData;
+  // }
+}
